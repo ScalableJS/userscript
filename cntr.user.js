5bab38052799ac93b59b27fc50373ef3434c1097
Oleh Cherkashchenko <oleh_cherkashchenko@epam.com>
2019-10-04 18:33:50 +0300

    test

--- cntr.user.js
+++ cntr.user.js
@@ -5,23 +5,28 @@
 // @description  try to take over the world!
 // @author       You
 // @match        https://sportchek.ca/*
-// @grant        none
+// @match        https://www.sportchek.ca/shopping-cart.html*
+// @grant        GM_webRequest
+// //@webRequest   [{"selector":"*shopping-cart*","action":"cancel"}]
+
 // ==/UserScript==
 
-(function() {
-    'use strict';
+(function () {
     SPC.trigger = (function (fn) {
-        return function(){
-            console.log('trigger', arguments)
+        return function () {
+            console.log('trigger', this.constructor.name)
             fn.apply(this, arguments)
         }
     }(SPC.trigger));
 
     SPC.subscribe = (function (fn) {
-        return function(){
-            console.log('subscribe', arguments)
+        return function () {
+            console.log('subscribe', this.constructor.name)
             fn.apply(this, arguments)
         }
-    }(SPC.subscribe))
-    // Your code here...
+    }(SPC.subscribe));
+
+    GM_webRequest([{"selector":"*shopping-cart*","action":"cancel"}], function(info, message, details) {
+        alert()
+    });
 })();
